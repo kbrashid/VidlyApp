2d50f229c3de1c54b84fcfa3d0879b2b68e48c9d
Khondoker Rashid <kbrashid@gmail.com>
2020-07-02 17:51:10 -0400

    TestUpdateAfterChangeOS

--- VidlyApp/Data/Migrations/20200625213712_PopulateMembershipTypes.cs
+++ VidlyApp/Data/Migrations/20200625213712_PopulateMembershipTypes.cs
@@ -6,11 +6,10 @@ namespace VidlyApp.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("Insert into MembershipTypes(Id, SignupFee, DurationInMonths, DiscountRate) Values (1, 0, 0, 0)");
-            migrationBuilder.Sql("Insert into MembershipTypes(Id, SignupFee, DurationInMonths, DiscountRate) Values (2, 30, 1, 10)");
-            migrationBuilder.Sql("Insert into MembershipTypes(Id, SignupFee, DurationInMonths, DiscountRate) Values (3, 90, 3, 15)");
-            migrationBuilder.Sql("Insert into MembershipTypes(Id, SignupFee, DurationInMonths, DiscountRate) Values (4, 300, 12, 20)");
-
+            migrationBuilder.Sql("Insert into MembershipTypes(Id, Name, SignupFee, DurationInMonths, DiscountRate) Values (1, N'PayAsYouGo', 0, 0, 0)");
+            migrationBuilder.Sql("Insert into MembershipTypes(Id, Name, SignupFee, DurationInMonths, DiscountRate) Values (2, N'Monthly', 30, 1, 10)");
+            migrationBuilder.Sql("Insert into MembershipTypes(Id, Name, SignupFee, DurationInMonths, DiscountRate) Values (3, N'Quarterly', 90, 3, 15)");
+            migrationBuilder.Sql("Insert into MembershipTypes(Id, Name, SignupFee, DurationInMonths, DiscountRate) Values (4, N'Annual', 300, 12, 20)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
---
+++ VidlyApp/Data/Migrations/20200702210252_PopulateMovie.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VidlyApp.Data.Migrations
+{
+    public partial class PopulateMovie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {            
+            migrationBuilder.Sql("INSERT INTO Movies([Id], [Name], [GenreId], [DateAdded], [ReleaseDate], [NumberInStock]) VALUES(1, N'Hangover', 2, N'2020-07-02 16:23:05', N'2019-04-05 00:00:00', 10)");
+            migrationBuilder.Sql("INSERT INTO Movies([Id], [Name], [GenreId], [DateAdded], [ReleaseDate], [NumberInStock]) VALUES(2, N'DieHard', 1, N'2020-07-02 16:23:30', N'2005-01-01 00:00:00', 10)");
+            migrationBuilder.Sql("INSERT INTO Movies([Id], [Name], [GenreId], [DateAdded], [ReleaseDate], [NumberInStock]) VALUES(3, N'Sonar Din', 4, N'2020-07-02 16:24:02', N'2017-01-12 00:00:00', 12)");
+            migrationBuilder.Sql("INSERT INTO Movies([Id], [Name], [GenreId], [DateAdded], [ReleaseDate], [NumberInStock]) VALUES(4, N'DebDas', 4, N'2020-07-02 16:24:42', N'2005-01-01 00:00:00', 12)");
+            migrationBuilder.Sql("INSERT INTO Movies([Id], [Name], [GenreId], [DateAdded], [ReleaseDate], [NumberInStock]) VALUES(5, N'Hard Target', 5, N'2020-07-02 16:25:25', N'2005-01-01 00:00:00', 12)");
+            migrationBuilder.Sql("INSERT INTO Movies([Id], [Name], [GenreId], [DateAdded], [ReleaseDate], [NumberInStock]) VALUES(6, N'Little Family', 3, N'2020-07-02 16:27:06', N'2019-04-05 00:00:00', 5)");
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
